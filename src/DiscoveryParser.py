ba40a71d362955aad56c40707098409b8d678be5
Robert Munteanu <robert.munteanu@gmail.com>
2011-04-19 23:36:46 +0300

    Implement parsing of request attributes

--- src/DiscoveryParser.py
+++ src/DiscoveryParser.py
@@ -3,12 +3,31 @@ class LogReader:
 	prefix = '/stats/mylyn/discovery'
 	prefixLength = len(prefix)
 
-	lines = []
+	entries = []
 
 	def readFile(self, logFile):
 		inp = open(logFile, "r")
 		for line in inp.readlines():
-			for field in line.split():
-				if ( field.startswith(self.prefix) ):
-					self.lines.append(field[self.prefixLength+1:])
+			self.parseLine(line)
 
+	def parseLine(self, line):
+		entry = LogEntry()
+		for field in line.split():
+			if ( field.startswith(self.prefix) ):
+				request = field[self.prefixLength+1:]
+				for key_value_pair in request[request.find('?')+1:].split('&'):
+					parsed = key_value_pair.split('=')
+					setattr(entry, parsed[0], parsed[1])
+		self.entries.append(entry)
+
+class LogEntry:
+
+	id = ''
+	discovery = ''
+	product = ''
+	buildId = ''
+	os = ''
+	arch = ''
+	ws = ''
+	nl = ''
+	timestamp = ''
--- test/test_parse.py
+++ test/test_parse.py
@@ -6,9 +6,16 @@ class LogParserTestCase(unittest.TestCase):
 		r = LogReader()
 		r.readFile('test/data/log.txt')
 		
-		self.assertEquals(4, len(r.lines))
-		self.assertEquals("com.foglyn?id=com.foglyn&discovery=3.5.0&product=com.adobe.flexbuilder.standalone.product&os=win32&arch=x86&ws=win32&nl=en_US", r.lines[0])
+		self.assertEquals(4, len(r.entries))
 
+		entry = r.entries[0];
+		self.assertEquals('com.foglyn', entry.id)
+		self.assertEquals('3.5.0', entry.discovery)
+		self.assertEquals('com.adobe.flexbuilder.standalone.product', entry.product)
+                self.assertEquals('win32', entry.os)
+                self.assertEquals('x86', entry.arch)
+                self.assertEquals('win32', entry.ws)
+                self.assertEquals('en_US', entry.nl)
 	
 if __name__ == "__main__":
     unittest.main()   
